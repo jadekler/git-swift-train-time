5f89b95cdc159850638e5560067c097fcd4cafa8
Jean de Klerk <jadekler@gmail.com>
2017-08-10 17:04:18 -0600

    Timer added

--- TrainTime/StatusMenuController.swift
+++ TrainTime/StatusMenuController.swift
@@ -4,15 +4,23 @@ class StatusMenuController: NSObject {
     @IBOutlet weak var statusMenu: NSMenu!
     
     let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    var timer: Timer?
+    var x = 0
     
     override func awakeFromNib() {
-        let icon = NSImage(named: "statusIcon")
-        icon?.isTemplate = true
-        statusItem.image = icon
+        statusItem.title = "TrainTime: ??"
         statusItem.menu = statusMenu
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTrainTime), userInfo: nil, repeats: true)
+    }
+    
+    func updateTrainTime() {
+        statusItem.title = "TrainTime: \(x)"
+        x += 1
     }
     
     @IBAction func quitClicked(_ sender: Any) {
+        timer?.invalidate()
         NSApplication.shared().terminate(self)
     }
 }
