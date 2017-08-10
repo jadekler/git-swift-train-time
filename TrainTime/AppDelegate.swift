88aedf4e9c290647fe25a0235443642ac346161a
Jean de Klerk <jadekler@gmail.com>
2017-08-10 15:58:53 -0600

    Initial Commit

---
+++ TrainTime/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TrainTime
+//
+//  Created by DeKlerk, Jean on 8/10/17.
+//  Copyright © 2017 DeKlerk, Jean. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TrainTime/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TrainTime
+//
+//  Created by DeKlerk, Jean on 8/10/17.
+//  Copyright © 2017 DeKlerk, Jean. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
